5d9216108c98ae350fca1be5e2b31db0dfe64a3d
zPonzio <mezaleandrodavid@gmail.com>
2026-07-03 18:43:32 -0300

    Modificación del Main
    Modifiqué el main
    Agregando una función aparte para el menu de estadísticas
    Agregué tmb manejo de errores en caso de que el usuario ingrese opciones incorrectas

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from persistencia import asegurar_directorio, cargar_datos, guardar_datos
-# Importamos los nuevos archivos/módulos que vamos a crear en los próximos pasos
 import libros as mod_libros
 import usuarios as mod_usuarios
 import prestamos as mod_prestamos
@@ -9,6 +8,23 @@ RUTA_DATOS = 'datos/'
 ARCHIVO_LIBROS = RUTA_DATOS + 'libros.txt'
 ARCHIVO_USUARIOS = RUTA_DATOS + 'usuarios.txt'
 ARCHIVO_PRESTAMOS = RUTA_DATOS + 'prestamos.txt'
+
+def menu_estadisticas(libros, usuarios, prestamos):
+    """Submenú de estadísticas del sistema."""
+    print("\n-- 📊 ESTADÍSTICAS --")
+    print("1. Estadísticas generales")
+    print("2. Top 3 libros con mayor stock")
+    print("3. Libros más y menos solicitados")
+    sub = input("Seleccione una opción: ")
+
+    if sub == '1':
+        mod_prestamos.mostrar_estadisticas_sistema(libros, usuarios, prestamos)
+    elif sub == '2':
+        mod_libros.top_libros_por_stock(libros)
+    elif sub == '3':
+        mod_prestamos.libros_mas_y_menos_solicitados(prestamos, libros)
+    else:
+        print("Opción no válida.")
 
 def menu_principal():
     """
@@ -25,7 +41,7 @@ def menu_principal():
 
     salir = False
     while not salir:
-        print("\n--- SISTEMA DE GESTIÓN BIBLIOTECARIA (REFACTORIZADO) ---")
+        print("\n--- SISTEMA DE GESTIÓN BIBLIOTECARIA ---")
         print("1. Gestión de Libros")
         print("2. Gestión de Usuarios")
         print("3. Gestión de Préstamos")
@@ -43,6 +59,8 @@ def menu_principal():
                 mod_libros.agregar_libro(libros)
             elif sub == '2': 
                 mod_libros.listar_libros(libros)
+            else:
+                print("Opción no válida.")
 
         elif opcion == '2':
             print("\n-- GESTIÓN DE USUARIOS --")
@@ -53,6 +71,8 @@ def menu_principal():
                 mod_usuarios.agregar_usuario(usuarios)
             elif sub == '2': 
                 mod_usuarios.listar_usuarios(usuarios)
+            else:
+                print("Opción no válida.")
 
         elif opcion == '3':
             print("\n-- GESTIÓN DE PRÉSTAMOS --")
@@ -66,20 +86,23 @@ def menu_principal():
                 mod_prestamos.registrar_devolucion_sistema(libros, prestamos)
             elif sub == '3': 
                 mod_prestamos.listar_prestamos(prestamos)
+            else:
+                print("Opción no válida.")
 
         elif opcion == '4':
-            # Delegamos el módulo de estadísticas a prestamos.py
-            mod_prestamos.mostrar_estadisticas_sistema(libros, usuarios, prestamos)
+            menu_estadisticas(libros, usuarios, prestamos)
 
         elif opcion == '5':
-            # Guardar todos los cambios definitivos al cerrar el programa
             guardar_datos(ARCHIVO_LIBROS, libros, mod_libros.CAMPOS_LIBROS)
             guardar_datos(ARCHIVO_USUARIOS, usuarios, mod_usuarios.CAMPOS_USUARIOS)
             guardar_datos(ARCHIVO_PRESTAMOS, prestamos, mod_prestamos.CAMPOS_PRESTAMOS)
             print("¡Cambios guardados con éxito! Sistema cerrado de forma segura.")
             salir = True
+            
         else:
             print("Opción no válida. Intente nuevamente.")
+
+
 
 if __name__ == "__main__":
     menu_principal()
